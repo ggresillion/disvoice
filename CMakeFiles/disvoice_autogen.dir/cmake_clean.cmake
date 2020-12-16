efd19a62fb1730678bd7f2eb8e30df20c39410e2
Guillaume GRESILLION <guillaume.gresillion@gmail.com>
2020-12-16 10:20:44 +0100

    working c++

---
+++ CMakeFiles/disvoice_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/disvoice_autogen"
+  "disvoice_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/disvoice_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
